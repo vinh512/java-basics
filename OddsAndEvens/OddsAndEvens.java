6b2d2032ebb2579a21d2e643334a3053b9236182
vinh huynh <vvvhuynh@yahoo.com>
2017-07-19 18:10:58 -0500

    asked for user input

--- OddsAndEvens/OddsAndEvens.java
+++ OddsAndEvens/OddsAndEvens.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 class OddsAndEvens {
 
   public static void main(String[] args) {
     System.out.println("Lets play a game called \"Odds and Evens\"");
+    System.out.print("What is your name? ");
+
+    Scanner input = new Scanner(System.in);
+    String name = input.nextLine();
+
+    System.out.print("Hi " + name + ", which do you prefer? (O)dds or (E)vens? ");
+    String choice = input.next();
+    choice = choice.toLowerCase();
+
+    if (choice.equals("o") || choice.equals("odds")) {
+      System.out.println(name + " has chosen Odds so the computer will be Evens.");
+    } else if (choice.equals("e") || choice.equals("evens")) {
+      System.out.println(name + " has chosen Evens so the computer will be Odd.");
+    }
+    System.out.println("------------------------------");
   }
 
 }
